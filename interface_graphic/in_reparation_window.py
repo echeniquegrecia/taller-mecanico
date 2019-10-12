2065b8c32fba4ca10cfd99123fff74b89faecc16
echeniquegrecia <echeniquegrecia@gmail.com>
2019-10-12 17:24:33 +0200

    creation in reparation window

--- interface_graphic/in_reparation_window.py
+++ interface_graphic/in_reparation_window.py
@@ -7,7 +7,35 @@ class InReparationWindow:
     """Class for window to repair"""
 
     def __init__(self):
-        print("test")
+        master = Tk()
+        listbox = Listbox(master, width=100, height=20)
+
+        listbox.grid(row=0, column=0, columnspan=4, padx=5, pady=5)
+
+        #listbox.pack(side="left", fill="y")
+        # scrollbar = Scrollbar(master, orient="vertical")
+        # scrollbar.config(command=listbox.yview)
+        # scrollbar.pack(side="right", fill="y")
+        # listbox.config(yscrollcommand=scrollbar.set)
+        #
+        #
+        list_values = ["one", "two", "three", "four", "hhh", "uuuu", "jjjj", "oooo", "mmmm", "tttt",
+                       "iii", "ggg", "ddd", "zzz", "aaaa"]
+        #
+        for item in list_values:
+            listbox.insert(END, item)
+        #
+        def testing():
+            value = listbox.curselection()
+            print(list_values[value[0]])
+            listbox.delete(ANCHOR)
+        #
+        #
+        #
+        btn = Button(master, text="Finalizar", command=testing)
+        btn.grid(row=1, column=0, columnspan=4, padx=5, pady=5)
+
+        mainloop()
 
 
 InReparationWindow()
